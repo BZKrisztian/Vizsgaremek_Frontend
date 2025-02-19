4c99b9ed44630d03fdb384860e6a97d391a475b2
Szakallas Kocsog <batkizoltankrisztian@ktch.hu>
2025-02-19 06:23:24 +0100

    TasklistCRUD_beg

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,16 +2,20 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Task } from '../models/task.model';
 import { HttpClient } from '@angular/common/http';
+import { TaskList } from '../models/tasklist.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
   private tasks: Task[] = [];
+  private tasklist: TaskList[] = [];
 
   private apiUrl = '';
 
   constructor(private http: HttpClient) {}
+
+
 
   getTasks(): Observable<Task[]> {
     return of(this.tasks);
@@ -35,6 +39,33 @@ export class TaskService {
     this.tasks = this.tasks.filter((task) => task.task_Id !== task_Id);
     return of(task_Id);
   }
+
+
+
+  getTaskLists(): Observable<TaskList[]> {
+    return of(this.tasklist)
+  }
+  addTaskList(tasklist: TaskList) {
+    tasklist.list_Id = this.tasklist.length + 1;
+    tasklist.creation_Date = new Date();
+    tasklist.update_Date = new Date();
+    this.tasklist.push(tasklist);
+    return of(tasklist);
+  }
+  updateTaskList(updatedTaskList: TaskList) {
+    this.tasklist = this.tasklist.map((tasklist) =>
+      tasklist.list_Id === updatedTaskList.list_Id
+        ? { ...updatedTaskList, update_Date: new Date() }
+        : tasklist
+    );
+    return of(updatedTaskList);
+  }
+  deleteTaskList(list_Id: number) {
+    this.tasklist = this.tasklist.filter((tasklist) => tasklist.list_Id !== list_Id);
+    return of(list_Id);
+  }
+
+
 
   register(userData: { email: string; password: string }) {
     return this.http.post(`${this.apiUrl}`, userData);
