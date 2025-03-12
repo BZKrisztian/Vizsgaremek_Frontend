a4eba72a717293c4aafe9d98041d3c515c521840
Erenyss <liesz.norbert@gmail.com>
2025-03-12 11:00:42 +0100

    made CSS changes, started to implement a show login button in enrty.components

--- src/app/pages/entry/entry.component.ts
+++ src/app/pages/entry/entry.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RegistrationComponent } from "../../components/registration/registration.component";
 import { LoginComponent } from "../../components/login/login.component";
 
@@ -9,10 +9,28 @@ import { LoginComponent } from "../../components/login/login.component";
   imports: [RegistrationComponent, LoginComponent]
 })
 export class EntryComponent implements OnInit {
-
-  constructor() { }
-
+  @ViewChild('btnlElement') btnlElement: ElementRef | undefined;
+  @ViewChild('loginElement') loginElement: ElementRef | undefined;
+  
+  constructor() { 
+    // this.btnlElement = document.getElementById("btn-l") as HTMLElement | null;
+    // this.loginElement = document.getElementById("app-login") as HTMLElement | null;
+    
+    // if (this.loginElement){
+    //   this.loginElement.style.display="none";
+    // }
+    
+    // if (this.btnlElement) {
+    //   this.btnlElement.addEventListener('click', this.openLogin);
+    // }
+  }
+  
   ngOnInit() {
   }
-
+  openLogin(): void {
+    if (this.loginElement){
+      this.loginElement.nativeElement.style.display = "block";
+      console.log("button pressed");
+    }
+  }
 }
