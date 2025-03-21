1e2b7b1305a77ade97109acf9bd4f20792c7b2fe
Szakallas Kocsog <batkizoltankrisztian@ktch.hu>
2025-03-21 13:39:17 +0100

    Set up translation, will expand/fix later

--- src/app/pages/entry/entry.component.ts
+++ src/app/pages/entry/entry.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-entry',
   templateUrl: './entry.component.html',
   styleUrls: ['./entry.component.css'],
-  imports: [RouterModule]
+  imports: [RouterModule, TranslateModule]
 })
 export class EntryComponent implements OnInit {
 
