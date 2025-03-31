477128491475c53450c197d4a99116077f49bab5
Lieszkofszki Norbert <lieszkofszki.norbert@gmail.com>
2025-04-01 01:15:46 +0200

    I did too many changes to list and tried to commit 9 times, should look better on mobile, added comments to CSS

---
+++ src/app/models/adminuser.model.ts
@@ -0,0 +1,8 @@
+export interface AdminUser {
+    adminUser_Id: number;
+    adminUserName: string;
+    adminPassword: string;
+    adminEmail: string;
+    adminAcc_CR_D: Date;
+    adminAcc_UP_D: Date;
+}
